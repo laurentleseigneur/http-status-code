8d66241db99911f2ffa5f76c57eb1f76c30b1595
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2024-05-16 10:42:03 +0200

    log interval

--- main.sh
+++ main.sh
@@ -47,6 +47,7 @@ function poll_status {
     if [[ "$username" != "" ]]; then
       auth="-u $username:$password"
     fi;
+    echo "interval set to [$interval]";
     STATUS_CODE=`curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" $auth $url -o /dev/null`
     echo "$(date +%H:%M:%S): The status code is $STATUS_CODE";
     if [[ "$STATUS_CODE" == "200" ]]; then
